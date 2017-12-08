1161f5f61970ea903ecb353ebfc870d21ff2bd2a
Paddym1991 <paddym1991.pm@gmail.com>
2017-12-08 09:45:30 +0000

    Refactor welcome to use open service.

--- app/src/main/java/app/donation/activity/Welcome.java
+++ app/src/main/java/app/donation/activity/Welcome.java
@@ -1,11 +1,8 @@
 package app.donation.activity;
 
 import app.donation.R;
-import app.donation.activity.Login;
-import app.donation.activity.Signup;
 import app.donation.main.DonationApp;
 import app.donation.model.Candidate;
-import app.donation.model.User;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -13,14 +10,15 @@ import retrofit2.Response;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
 import java.util.List;
 
-
-public class Welcome extends AppCompatActivity implements Callback<List<User>>
+/**
+ * The welcome activity can be simplified, as we are no longer downloading a full list of users
+ */
+public class Welcome extends AppCompatActivity
 {
   private DonationApp app;
 
@@ -33,57 +31,25 @@ public class Welcome extends AppCompatActivity implements Callback<List<User>>
     app = (DonationApp) getApplication();
   }
 
-  /**
-   * issuing a request to the API for the list of all donors
-   */
   @Override
   public void onResume()
   {
     super.onResume();
-    app.currentUser = null;
-    Call<List<User>> call1 = (Call<List<User>>) app.donationService.getAllUsers();
-    call1.enqueue(this);
+    app.currentUser = null;;
 
-    Call<List<Candidate>> call2 = (Call<List<Candidate>>) app.donationService.getAllCandidates();
-    call2.enqueue(new Callback<List<Candidate>>() {
+    Call<List<Candidate>> call = (Call<List<Candidate>>) app.donationServiceOpen.getAllCandidates();
+    call.enqueue(new Callback<List<Candidate>>() {
       @Override
       public void onResponse(Call<List<Candidate>> call, Response<List<Candidate>> response) {
+        serviceAvailableMessage();
         app.candidates = response.body();
       }
 
       @Override
       public void onFailure(Call<List<Candidate>> call, Throwable t) {
-        app.donationServiceAvailable = false;
         serviceUnavailableMessage();
       }
     });
-
-  }
-
-  /**
-   * Successful response to API request
-   * @param call
-   * @param response
-   */
-  @Override
-  public void onResponse(Call<List<User>> call, Response<List<User>> response)
-  {
-    serviceAvailableMessage();
-    app.users = response.body();
-    //Log.v("donation", app.users.toString());
-    app.donationServiceAvailable = true;
-  }
-
-  /**
-   * Error response to API request
-   * @param call
-   * @param t
-   */
-  @Override
-  public void onFailure(Call<List<User>> call, Throwable t)
-  {
-    app.donationServiceAvailable = false;
-    serviceUnavailableMessage();
   }
 
   public void loginPressed (View view)
@@ -112,12 +78,14 @@ public class Welcome extends AppCompatActivity implements Callback<List<User>>
 
   void serviceUnavailableMessage()
   {
+    app.donationServiceAvailable = false;
     Toast toast = Toast.makeText(this, "Donation Service Unavailable. Try again later", Toast.LENGTH_LONG);
     toast.show();
   }
 
   void serviceAvailableMessage()
   {
+    app.donationServiceAvailable = true;
     Toast toast = Toast.makeText(this, "Donation Contacted Successfully", Toast.LENGTH_LONG);
     toast.show();
   }
