dddea714044cb67558bb22a19eb1bbfcd8f6125f
Paddym1991 <paddym1991.pm@gmail.com>
2017-12-08 09:43:54 +0000

    Refactored DonationApp to use new service factory + remove local users collection.

--- app/src/main/java/app/donation/main/DonationApp.java
+++ app/src/main/java/app/donation/main/DonationApp.java
@@ -7,33 +7,30 @@ import android.app.Application;
 import android.util.Log;
 import android.widget.Toast;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-
+import app.donation.model.Token;
 import app.donation.model.Candidate;
 import app.donation.model.User;
 import app.donation.model.Donation;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 /**
- * This version sets up a DonationService object - and makes this service object available for the activities
- * It also introduces a candidates collection and a url we should use to access the remote service
+ * DonationApp class in restructured now to use the new authenticate route - encapsulated in the validUser and onResponse
  */
-public class DonationApp extends Application
+public class DonationApp extends Application implements Callback<Token>
 {
-  public DonationService donationService;
+  public DonationServiceOpen donationServiceOpen;
+  public DonationService     donationService;
+
   public boolean         donationServiceAvailable = false;
-  public String          service_url  = "http://10.0.2.2:4000";   // Standard Emulator IP Address : use this one when app is secured
-  //public String          service_url  = "http://10.8.89.59:4000";   // Standard Emulator IP Address
- // public String          service_url  = "http://10.0.2.2:9000";   // Standard Emulator IP Address
+  public String          service_url  = "http://10.0.2.2:4000";   // Standard Emulator IP Address
 
   public final int       target       = 10000;
   public int             totalDonated = 0;
 
   public User             currentUser;
   public List <Donation>  donations    = new ArrayList<Donation>();
-  public List <User>      users        = new ArrayList<User>();
   public List <Candidate> candidates   = new ArrayList<Candidate>();
 
   public boolean newDonation(Donation donation)
@@ -56,34 +53,32 @@ public class DonationApp extends Application
   public void onCreate()
   {
     super.onCreate();
-    super.onCreate();
-    Gson gson = new GsonBuilder().create();
-
-    Retrofit retrofit = new Retrofit.Builder()
-            .baseUrl(service_url)
-            .addConverterFactory(GsonConverterFactory.create(gson))
-            .build();
-    donationService = retrofit.create(DonationService.class);
-
+    donationServiceOpen = RetrofitServiceFactory.createService(DonationServiceOpen.class);
     Log.v("Donation", "Donation App Started");
-  }
-
-  public void newUser(User user)
-  {
-    users.add(user);
   }
 
   public boolean validUser (String email, String password)
   {
-    for (User user : users)
-    {
-      if (user.email.equals(email) && user.password.equals(password))
-      {
-        currentUser = user;
-        return true;
-      }
-    }
-    return false;
+    User user = new User ("", "", email, password);
+    donationServiceOpen.authenticate(user);
+    Call<Token> call = (Call<Token>) donationServiceOpen.authenticate (user);
+    call.enqueue(this);
+    return true;
   }
 
+  //creating the closed route proxy using the token we have just received (if we had the correct credentials)
+  @Override
+  public void onResponse(Call<Token> call, Response<Token> response) {
+    Token auth = response.body();
+    currentUser = auth.user;
+    donationService =  RetrofitServiceFactory.createService(DonationService.class, auth.token);
+    Log.v("Donation", "Authenticated " + currentUser.firstName + ' ' + currentUser.lastName);
+  }
+
+  @Override
+  public void onFailure(Call<Token> call, Throwable t) {
+    Toast toast = Toast.makeText(this, "Unable to authenticate with Donation Service", Toast.LENGTH_SHORT);
+    toast.show();
+    Log.v("Donation", "Failed to Authenticated!");
+  }
 }
